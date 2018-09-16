6a6c9a55649f05aa79a4066fee0407bd5689b73f
Tran Manh Cuong <iammaytinhdibo@gmail.com>
2018-09-16 15:50:33 +0700

    fix

--- index.js
+++ index.js
@@ -139,11 +139,6 @@ function sendBook(sender_psid) {
                     "type":"web_url",
                     "url":"https://www.messeng.com",
                     "title":"Bài tập thực hành"
-                  },
-                  {
-                    "type":"web_url",
-                    "url":"https://www.messe.com",
-                    "title":"Nội dung bài tập lớn"
                   }
                 ]  
                 
